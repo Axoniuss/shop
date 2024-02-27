f1831c432fd1230a1ea746418b54a2167545c63b
Axoniuss <klim.vlad@list.ru>
2024-02-27 11:07:41 +0300

    routes.php

--- vendor/wfm/Router.php
+++ vendor/wfm/Router.php
@@ -27,7 +27,49 @@ class Router
 
     public static function dispatch($url)
     {
-        var_dump($url);
+    //    var_dump($url);
+        if (self::matchRoute($url)) {
+            echo 'OK';
+        } else {
+            echo 'NO';
+        }
+    }
+    public static function matchRoute($url): bool
+    {
+        debug(self::$route);
+        foreach (self::$routes as $pattern => $route) {
+            if (preg_match("#{$pattern}#", $url, $matches)) {
+                foreach ($matches as $k => $v) {
+                    if (is_string($k)) {
+                        $route[$k] = $v;
+                    }
+                }
+                debug($route);
+                if (empty($route['action'])) {
+                    $route['action'] = 'index';
+                }
+                if (!isset($route['admin_prefix'])) {
+                    $route['admin_prefix'] = '';
+                } else {
+                    $route['admin_prefix'] = '\\';
+                }
+
+                debug($route);
+                return true;
+            }
+        }
+        return false;
+    }
+    // CamelCase
+    protected static function upperCamelCase($name): string
+    {
+        return str_replace(' ', '', ucwords(str_replace('-', ' ', $name)));
+    }
+
+    // camelCase
+    protected static function lowerCamelCase($name): string
+    {
+        return lcfirst(self::upperCamelCase($name));
     }
 
 }
